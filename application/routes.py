aed2187233aa60180f394f7c00fe33debaafb729
Bhupinder Singh <khanujabhupinder09@gmail.com>
2020-01-31 11:13:44 +0530

    Changes in CSS and HTML

--- application/routes.py
+++ application/routes.py
@@ -4,7 +4,6 @@ from application.forms import LoginForm
 from application.models import Emp
 from flask_login import login_user,current_user
 
-@app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
@@ -17,6 +16,7 @@ def dashboard():
 def files():
     return render_template("files.html")
 
+@app.route('/', methods=['GET', 'POST'])
 @app.route("/login", methods=['GET','POST'])
 def login():
 	if current_user.is_authenticated:
@@ -24,7 +24,7 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		emp = Emp.query.filter_by(empname=form.username.data).first()
-		if emp and (emp.emppass, form.password.data):
+		if emp and (emp.emppass == form.password.data):
 			login_user(emp, remember=form.remember.data)
 			return redirect(url_for('home'))
 		else:
